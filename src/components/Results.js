71790a90da192938578c3f774c600a1a3fb2fbbc
ezra mizrahi <mishraki@gmail.com>
2017-12-13 08:37:28 +1100

    updates to flight component

--- src/components/Results.js
+++ src/components/Results.js
@@ -3,7 +3,7 @@ import React, { PureComponent as Component } from 'react';
 function Results(props) {
   return (
     <div>
-      { props.flights.map( f => <p key={ f.id }>{ f.flight_num }</p>)}
+      { props.flights.map( f => <p key={ f.id }>{ f.origin  }</p> ) }
     </div>
   );
 }
