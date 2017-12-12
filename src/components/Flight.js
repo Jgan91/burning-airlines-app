0a81964a4c2e81b52cc74953efd26ed11a4f36a3
ezra mizrahi <mishraki@gmail.com>
2017-12-12 16:30:54 +1100

    basics of flight component

---
+++ src/components/Flight.js
@@ -0,0 +1,14 @@
+import React, { PureComponent as Component } from 'react';
+import BurningAirlines from './BurningAirlines';
+
+class Flight extends Component {
+  render() {
+    return (
+      <div>
+        <h1>flights soon</h1>
+      </div>
+    )
+  }
+}
+
+export default Flight;
