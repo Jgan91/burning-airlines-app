2f7530ebc3615f1aeaae15e97cc712c0862d4ed4
JustinGan <jgan91@gmail.com>
2017-12-13 18:45:34 +1100

    moved state up to Reservation

--- src/components/Reservation.js
+++ src/components/Reservation.js
@@ -1,17 +1,29 @@
 import React, { PureComponent as Component } from 'react';
 import Flight from './Flight';
 import { Link } from 'react-router-dom';
+
 import axios from 'axios';
+import _ from 'underscore';
 
 const SERVER_URL = 'http://localhost:5000/flights/'
 
 class Reservation extends Component {
+  constructor( props ) {
+    super( props );
+    this.state = { flight: '' };
+
+    const fetchFlights = () => {
+      axios.get( SERVER_URL + this.props.match.params.flightId +".json" ).then( results => this.setState( { flight: results.data } ) );
+    }
+    fetchFlights();
+  }
+
   render() {
     return (
     <div>
       <h1> Reservation </h1>
       < FlightInfo flightId={ this.props.match.params.flightId } />
-      < SeatingPlan />
+      < SeatingPlan flightId={ this.props.match.params.flightId } />
       < SelectedSeat />
       <h2>{ this.props.match.params.flightId }</h2>
     </div>
@@ -25,7 +37,6 @@ class FlightInfo extends Component {
     this.state = { flight: '' };
 
     const flightId = props.flightId
-    console.log( flightId );
     const fetchFlights = () => {
       axios.get( SERVER_URL+ props.flightId +".json" ).then( results => this.setState( { flight: results.data } ) );
     }
@@ -44,11 +55,29 @@ class FlightInfo extends Component {
 }
 
 class SeatingPlan extends Component {
+  constructor( props ) {
+    super( props );
+    this.state = { airplane: {} };
 
+    const fetchAirplane = () => {
+      axios.get( SERVER_URL + props.flightId ).then( results => this.setState( { airplane: results.data.airplane } ) );
+    }
+    fetchAirplane();
+  }
 
   render() {
+    const columns = this.state.airplane.columns;
+    const rows = this.state.airplane.rows;
+
     return (
-      <h1> Seating plan coming soon </h1>
+      <div>
+        <h1> Seating plan coming soon </h1>
+        <div className="plan">
+          { this.state.airplane.name }
+          { this.state.airplane.columns }
+
+        </div>
+      </div>
     )
   }
 }
